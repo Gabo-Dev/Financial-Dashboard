bd8d0d9498df0d044077dbe3b9be6aa91e6a1c53
Gabo-Dev <52703336+Gabo-Dev@users.noreply.github.com>
2024-10-02 12:41:11 +0200

    SliderBar Function added

--- DashboardApp/src/App.jsx
+++ DashboardApp/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductsPage from "./pages/ProductsPage";
+import UsersPage from "./pages/UsersPage";
+import SalesPage from "./pages/SalesPage";
+import OrdersPage from "./pages/OrdersPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
+import SettingsPage from "./pages/SettingsPage";
 
+import Sidebar from "./components/Sidebar";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      <div className="fixed inset-0 z-0">
+        <div
+          className="absolute inset-0 bg-gradient-to-br from-gray-900
+           via-gray-800 to-gray-900 opacity-80"
+        />
+        <div className="absolute inset-0 backdrop-blur-sm" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/Users" element={<UsersPage />} />
+        <Route path="/Sales" element={<SalesPage />} />
+        <Route path="/Orders" element={<OrdersPage />} />
+        <Route path="/Analytics" element={<AnalyticsPage />} />
+        <Route path="/Settings" element={<SettingsPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
