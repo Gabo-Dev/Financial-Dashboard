a74dac8a54b72d6ba67d3c7d5b653052e97a975d
Gabo-Dev <52703336+Gabo-Dev@users.noreply.github.com>
2024-10-10 18:13:52 +0200

    update chart

--- DashboardApp/src/components/overview/CurrenciesOverviewChart.jsx
+++ DashboardApp/src/components/overview/CurrenciesOverviewChart.jsx
@@ -39,10 +39,10 @@ function SalesOverviewChart() {
       </h2>
       <div className="h-80">
         <ResponsiveContainer width="100%" height="100%">
-          <LineChart data={chartData}>
-            <CartesianGrid strokeDasharray="3 3" stroke="#6366f1" />
-            <XAxis dataKey="date" />
-            <YAxis  domain={["auto", "auto"]} />
+          <LineChart data={chartData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid stroke="#6366f1" strokeDasharray="4" horizontal={false}  vertical={false}/>
+            <XAxis dataKey="date"  />
+            <YAxis   domain={["auto", "auto"]}/>
             <Tooltip viewBox={{ x: 0, y: 0, width: 200, height: 50 }} contentStyle={{color: "orange"}}/>
             <Line type="monotone" dataKey="open" stroke="#8884d8"  dot={{r: 6}}  activeDot={{ stroke: 'red', strokeWidth: 2, r: 10 }}/>
           </LineChart>
